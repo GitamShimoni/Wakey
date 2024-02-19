20cb43637df9cb5e798a26b941948dfff33fa8e3
SpeedyQ1 <sahar.y.ash@gmail.com>
2024-02-19 22:40:25 +0200

    sahar last day

--- src/Components/MainPage.jsx
+++ src/Components/MainPage.jsx
@@ -4,9 +4,13 @@ import "animate.css";
 import InTrip from "./InTrip";
 import axios from "axios";
 import PlanTrip from "./PlanTrip";
+import SettingsIcon from '@mui/icons-material/Settings';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import { useNavigate } from 'react-router-dom';
 
 function MainPage() {
   const [isUserSleepingBool, setIsUserSleepingBool] = useState(false);
+  const navigate = useNavigate();
 
   async function isUserSleeping() {
     console.log("Callingggg");
@@ -26,8 +30,10 @@ function MainPage() {
   return (
     <div id="mainPage-container">
       {isUserSleepingBool ? <InTrip /> : <PlanTrip />}
+      <div className="settings-btn" onClick={()=> navigate("/settings")}><AccountCircleIcon/></div>
     </div>
   );
+
 }
 
 export default MainPage;
